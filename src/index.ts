fc5a4a9da9555d61d05163a0423677cbebea987f
Ramitsan <ramitsan@gmail.com>
2023-06-30 23:09:42 +0300

    feat: add webpack

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import {foo} from './script';
+import { foo } from './script';
 import http from 'http';
 
 const a1: number = 1;
 console.log(a1);
 console.log(foo());
 
-const server = http.createServer((request, resp) => {
+const users = [
+  {
+    username: 'Ivan'
+  },
+  {
+    username: 'Alex'
+  }
+];
 
+const server = http.createServer((request, resp) => {
+  console.log(request.url, request.method);
+  resp.statusCode = 200;
+  resp.end(JSON.stringify(users));
 })
 
 server.listen(4000);
